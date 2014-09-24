5070da4359e1f5a24fc245a29bdc7d0395919d30
Ganapati <ganapati@bobib.fr>
2014-09-24 09:31:11 +0200

    Change license to beerware

--- wprecon.py
+++ wprecon.py
@@ -1,3 +1,12 @@
+"""
+----------------------------------------------------------------------------
+"THE BEER-WARE LICENSE" (Revision 42):
+ganapati (@G4N4P4T1) wrote this file. As long as you retain this notice you
+can do whatever you want with this stuff. If we meet some day, and you think
+this stuff is worth it, you can buy me a beer in return Poul-Henning Kamp
+----------------------------------------------------------------------------
+"""
+
 from bs4 import BeautifulSoup
 import re
 import requests
--- wpyscan.py
+++ wpyscan.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python
+"""
+----------------------------------------------------------------------------
+"THE BEER-WARE LICENSE" (Revision 42):
+ganapati (@G4N4P4T1) wrote this file. As long as you retain this notice you
+can do whatever you want with this stuff. If we meet some day, and you think
+this stuff is worth it, you can buy me a beer in return.
+----------------------------------------------------------------------------
+"""
 
 import argparse
 from wprecon import WPRecon
